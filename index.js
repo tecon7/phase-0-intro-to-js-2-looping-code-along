58f38d9ff6c0481b14356bfe09a1b27b26ce748d
Teddy Economou <teddyecon@gmail.com>
2022-12-01 11:52:01 -0500

    commit tree 9b528e69bf3b674830eebcd829961318a6bc25fe

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    const message = [];
+        for (let i=0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown(startingPoint) {
+    while (startingPoint >=0)
+    {console.log(startingPoint--)};
+}
+// countDown(38)
